01ba2c437267ba60d490e5e2b7a6fca508a906d0
VishalParvannan <vishalparvannan.vp4@gmail.com>
2025-07-22 20:48:48 -0400

    Add files via upload

---
+++ VariableInitialization.java
@@ -0,0 +1,14 @@
+public class VariableInitialization {
+    public static void main(String[] args) {
+        
+        /*
+         *  1. The money in your bank account -> double
+            2. The sum of a series of natural numbers -> int
+            3. A single letter -> char
+            4. The robot’s name -> String
+            5. The volume of a sphere -> double
+            6. The number of neurons in your brain -> long
+         * 
+         */
+    }
+}
